98147a9d649bd56701b22ccbb55ca8b04188f37a
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-15 22:11:08 +0200

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,6 +1,7 @@
 import cmath
-def calc_triangle_area(length1, length2, length3):
-    s= (1/2)*(length1+ length2+ length3) 
-    area= cmath.sqrt(s*(s-length1)*(s-length2)*(s-length3))
+def calc_triangle_area(length_one, length_two, length_three):
+    s= (1/2)*(length_one+ length_two+ length_three) 
+    area= cmath.sqrt(s*(s-length_one)*(s-length_two)*(s-length_three))
     return area
+
 print(calc_triangle_area(10,5,4))
