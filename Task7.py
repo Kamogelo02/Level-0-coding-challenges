a2cfd758d7ac77638a52c6b6c210bde626438a80
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-15 19:40:48 +0200

    Update Task7.py

--- Task7.py
+++ Task7.py
@@ -1,9 +1,9 @@
 def celsius_to_fahrenheit(celsius):
     fahrenheit= (celsius*1.8) +32
     return fahrenheit
-celsius_to_fahrenheit(23)
+print(celsius_to_fahrenheit(23))
 
 def fahrenheit_to_celsius(fahrenheit):
     celsius= (fahrenheit -32) *(5/9)
     return celsius
-fahrenheit_to_celsius(23)
+print(fahrenheit_to_celsius(23))
