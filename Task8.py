9e516a9eef35f846afaf6016711cad32de66bb8b
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-15 18:38:25 +0200

    Update Task8.py

--- Task8.py
+++ Task8.py
@@ -3,12 +3,13 @@ def convert_into_hours_and_minutes(number):
     minutes= int(number%60)
     hours= int((number-minutes)/60)
     if(hours>1):
-        output= str(hours)+ " hours, "
+        output= f"{hours} hours, "
     else:
-        output=str(hours)+ " hour, "
+        output= f"{hours} hour, "
     if(minutes>1):
-        output= output+ str(minutes)+ " minutes."
+        output= f"{output}{minutes} minutes."
     else:
-        output=output +str(minutes) + " minute."
+        output=f"{output}{minutes} minute."
     return output
+
 print(convert_into_hours_and_minutes(71))
