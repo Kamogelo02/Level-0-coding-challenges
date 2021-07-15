8052e39b23a516acd8dfff1071223f7fe292b917
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-15 22:07:05 +0200

    Update Task10.py

--- Task10.py
+++ Task10.py
@@ -1,6 +1,7 @@
-def common_letters(string1,string2):
-    words= list(set(string1) &set(string2))
+def common_letters(string_one,string_two):
+    words= list(set(string_one) &set(string_two))
     print("Common letters: ", end=" ")
     for letter in words:
-      print(letter, end= ", ") 
+      print(letter, end= ", ")
+    
 common_letters("house", "computers")
