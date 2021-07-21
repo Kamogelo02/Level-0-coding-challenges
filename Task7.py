9685fbdb063c50c732d949d32864aa05f0f988b8
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-21 13:04:40 +0200

    Added name/main function

--- Task7.py
+++ Task7.py
@@ -1,9 +1,16 @@
 def celsius_to_fahrenheit(celsius):
     fahrenheit= (celsius*1.8) +32
     return fahrenheit
-print(celsius_to_fahrenheit(23))
+
+def main():
+    print(celsius_to_fahrenheit(23))
 
 def fahrenheit_to_celsius(fahrenheit):
     celsius= (fahrenheit -32) *(5/9)
     return celsius
-print(fahrenheit_to_celsius(23))
+    
+def main():
+    print(fahrenheit_to_celsius(23))
+
+if __name__=="__main__":
+    main()
