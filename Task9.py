16c9509bc69050bc8a8040e2a423fde977e0b92d
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-21 13:09:06 +0200

    Correct expected output

--- Task9.py
+++ Task9.py
@@ -1,7 +1,18 @@
 def vowel(string):
-    vowels= "aeiouAEIOU"
-    print("Vowels: ", end="")
+    vowels= ['a', 'e', 'i', 'o', 'u']
+    output= list()
+    
     for letters in string:
-        if letters in vowels:
-            print(letters, end =", ")
-vowel("Umuzi")
+        if (letters in vowels) and (letters not in output):
+            output.append(letters)
+    
+    output_str=str(output)[1:-1]
+    new_str_output= output_str.replace("'", "")
+
+    print(new_str_output)
+
+def main():
+    vowel("Umuzi")
+
+if __name__=="__main__":
+    main()
