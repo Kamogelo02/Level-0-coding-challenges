61f111e0976107a79c055fb97d2143e3030f956d
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-26 00:41:45 +0200

    Update and rename Task7.py to Task0_7.py

--- Task0_7.py
+++ Task0_7.py
@@ -2,14 +2,12 @@ def celsius_to_fahrenheit(celsius):
     fahrenheit= (celsius*1.8) +32
     return fahrenheit
 
-def main():
-    print(celsius_to_fahrenheit(23))
-
 def fahrenheit_to_celsius(fahrenheit):
     celsius= (fahrenheit -32) *(5/9)
     return celsius
     
 def main():
+    print(celsius_to_fahrenheit(23))
     print(fahrenheit_to_celsius(23))
 
 if __name__=="__main__":
