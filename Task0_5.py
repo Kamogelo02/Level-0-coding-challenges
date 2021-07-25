c762dcd2a6ca10674766ad6ce30d066aa6c5dba0
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-26 00:39:08 +0200

    Update and rename Task5.py to Task0_5.py

--- Task0_5.py
+++ Task0_5.py
@@ -1,12 +1,13 @@
 import cmath
 
 def calc_triangle_area(length1, length2, length3):
+
     s= (1/2)*(length1+ length2+ length3) 
-    area= cmath.sqrt(s*(s-length1)*(s-length2)*(s-length3))
+    area= (s*(s-length1)*(s-length2)*(s-length3))**0.5
     return area
 
 def main():
-    print(calc_triangle_area(10,5,4))
+    print(calc_triangle_area(3,5,4))
 
 if __name__=="__main__":
     main()
