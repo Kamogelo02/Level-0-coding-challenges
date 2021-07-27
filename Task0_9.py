e4f3c959217b264c01a034712ad58ae02e1b054f
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-27 11:30:16 +0200

    Update Task0_9.py

--- Task0_9.py
+++ Task0_9.py
@@ -1,19 +1,21 @@
 def vowel(string):
-    vowels= ['a', 'e', 'i', 'o', 'u']
-    output= list()
-    string= string.lower()
-    
+    vowels = ["a", "e", "i", "o", "u"]
+    output = list()
+    string = string.lower()
+
     for letters in string:
         if (letters in vowels) and (letters not in output):
             output.append(letters)
-     
-    output_str=str(output)[1:-1]
-    new_str_output= output_str.replace("'", "")
 
-    print(new_str_output)
+    output_str = str(output)[1:-1]
+    new_str_output = output_str.replace("'", "")
+
+    print(f"Vowels: {new_str_output}.")
+
 
 def main():
     vowel("Umuzi")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
