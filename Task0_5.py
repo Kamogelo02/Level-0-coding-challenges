82f962ef2fd56ddc235f9f22a96bd9660faa3710
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-07-27 18:36:30 +0200

    Update Task0_5.py

--- Task0_5.py
+++ Task0_5.py
@@ -1,10 +1,18 @@
 def calc_triangle_area(length1, length2, length3):
-    s= (1/2)*(length1+length2+length3)
-    area= (s*(s-length1)*(s-length2)*(s-length3))**0.5
+    semi_parameter = (1 / 2) * (length1 + length2 + length3)
+    area = (
+        semi_parameter
+        * (semi_parameter - length1)
+        * (semi_parameter - length2)
+        * (semi_parameter - length3)
+    ) ** 0.5
     return area
 
-def main():
-    print(calc_triangle_area(3,5,4))
 
-if __name__=="__main__":
+def main():
+    print(calc_triangle_area(3, 5, 4))
+
+
+if __name__ == "__main__":
     main()
+
