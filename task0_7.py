34aea38948259750fc516d037ff2aa5273ebc478
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-08-02 17:25:15 +0200

    Update task0_7.py

--- task0_7.py
+++ task0_7.py
@@ -1,14 +1,17 @@
 def celsius_to_fahrenheit(celsius):
-    fahrenheit= (celsius*1.8) +32
+    fahrenheit = (celsius * 1.8) + 32
     return fahrenheit
 
+
 def fahrenheit_to_celsius(fahrenheit):
-    celsius= (fahrenheit -32) *(5/9)
+    celsius = (fahrenheit - 32) * (5 / 9)
     return celsius
-    
+
+
 def main():
     print(celsius_to_fahrenheit(23))
     print(fahrenheit_to_celsius(23))
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
