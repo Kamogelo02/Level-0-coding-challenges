62495da77c6832b8423249dcafe34718a257e8c1
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-08-02 02:57:58 +0200

    Update task0_4.py

--- task0_4.py
+++ task0_4.py
@@ -1,11 +1,13 @@
-def even_or_odd(x):
-  if x % 2==0:
-    print("even")
-  else:
-    print("odd")
+def even_or_odd(number):
+    if number % 2 == 0:
+        print("even")
+    else:
+        print("odd")
 
 def main():
-  even_or_odd(5)
+    even_or_odd(5)
 
-if __name__=="__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
+
