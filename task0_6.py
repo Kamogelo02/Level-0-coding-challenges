280caaaa4465eabd9251a2960994c31b507524f2
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-08-02 17:24:20 +0200

    Update task0_6.py

--- task0_6.py
+++ task0_6.py
@@ -1,12 +1,14 @@
 def maximum(*numbers):
-    largest= numbers[0]
+    largest = numbers[0]
     for x in numbers:
-        if x>largest:
-            largest=x   
+        if x > largest:
+            largest = x
     return largest
 
-def main():
-    print(maximum(1,3,6,200,90))
 
-if __name__=="__main__":
-    main() 
+def main():
+    print(maximum(1, 3, 6, 200, 90))
+
+
+if __name__ == "__main__":
+    main()
