d308450cacb802f4d42ef536f28dd5237920aeb4
Kamogelo02 <77056865+Kamogelo02@users.noreply.github.com>
2021-08-02 16:32:13 +0200

    Update task0_10.py

--- task0_10.py
+++ task0_10.py
@@ -1,17 +1,19 @@
-def common_letters(string1,string2):
-    output= []
-    string1= string1.lower()
-    string2= string2.lower()
-    
-    for letter in string1:
-      if letter in string2:
-        output.append(letter)
-    
-    output= ', '.join(output)
+def common_letters(string1, string2):
+    output = []
+    string1 = string1.lower()
+    string2 = string2.lower()
+
+    for letter in set(string1):
+        if letter in set(string2):
+            output.append(letter)
+
+    output = ", ".join(output)
     print(f"Common letters: {output}")
 
-def main():      
-    common_letters("Eckard", "computers")
 
-if __name__=="__main__":
-  main()
+def main():
+    common_letters("EckardR", "Berry")
+
+
+if __name__ == "__main__":
+    main()
